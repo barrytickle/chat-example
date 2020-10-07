c24bc3f8e23f08e9b0ee2691dc2ae4918254c17f
Barry Tickle <barry.tickle12@gmail.com>
2020-10-07 23:18:58 +0100

    First inital commit

---
+++ config.js
@@ -0,0 +1,7 @@
+const gateway = {
+    app: 'http://localhost:9000',
+    socket: 'http://localhost:5000',
+    client: 'http://localhost:3000'
+}
+
+module.exports = {gateway};
--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
+var faker = require('faker');
+var axios = require('axios');
+var path = require('path');
+var {gateway} = require('./config');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+console.log(gateway);
+let appData = '';
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  axios.get(`${gateway.app}/sockettest`)
+      .then(async function(response){
+        appData = await response.data;
+      })
+      .catch(function(error){
+        console.warn(error)
+      })
+      .then(function(){
+      });
+
+  res.render('index', {data: JSON.stringify(appData)});
+});
+
+app.post('/data', function(req, res){
+  console.log(appData !== '' ? appData : 'App Data is empty');
+  const messageContent = {
+    "host": faker.name.findName(),
+    "client": faker.name.findName(),
+    "message":faker.lorem.words(15)
+  }
+ io.sockets.emit('chat message', JSON.stringify(messageContent));
 });
 
 io.on('connection', function(socket){
